43591895dcde886711cc09feafacef18e064c771
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-12-07 16:58:21 +0100

    main to Game

--- Problem liszaja/Game.h
+++ Problem liszaja/Game.h
@@ -1,5 +1,15 @@
 #pragma once
-class Game
-{
-};
+#include <SFML/Graphics.hpp>
+#include "Board.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <SFML/Window.hpp>
 
+class Game {
+public:
+    int run();
+    void delay(int time, Board* board, sf::RenderWindow& window);
+private:
+
+};
