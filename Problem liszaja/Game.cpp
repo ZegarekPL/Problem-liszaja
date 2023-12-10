98be1674d9a442c1c6b7bd60d470b5543340f498
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-12-10 12:08:44 +0100

    update

--- Problem liszaja/Game.cpp
+++ Problem liszaja/Game.cpp
@@ -6,7 +6,6 @@ int Game::run() {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Problem liszaja", sf::Style::Titlebar | sf::Style::Close);
 
     bool gameStarted = false;
-    bool gamePause = true;
     bool menuOpen = true;
     int currentround = 1;
 
@@ -15,65 +14,62 @@ int Game::run() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-            }
-
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::W) {
-                if (menuOpen && gamePause) {
-                    menuOpen = false;
-                    gamePause = false;
-                }
-                else if (!menuOpen && !gamePause) {
-                    menuOpen = true;
-                    gamePause = true;
-                }
-            }
+            }  
 
             if (menuOpen) {
                 window.clear();
                 menu.drawMenu(window);
                 window.display();
             }
-            else {
-                if (!gameStarted) {
-                    gameStarted = true;
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::W) {
+                if (menuOpen) {
+                    menuOpen = false;
+                    break;
                 }
-                
-                if (currentround == 1) {
-                    window.clear();
-                    board.calculateboardSize(window);
-                    board.handleClick(currentround, window);
-                    board.drawBoard(window);
-                    window.display();
-                    currentround = 2;
+                else if (!menuOpen) {
+                    menuOpen = true;
+                    break;
                 }
-
-                if (currentround != 1 && currentround <= duration.maxround) {
-                    if (board.countCells(Health, board.size) == board.size * board.size) {
-                        cout << "Koniec Gry: Wszystkie komorki zdrowe" << endl;
-                        duration.delay(5, window);
-                        window.close();
-                        break;
-                    }
-
-                    if (board.countCells(Infected, board.size) == board.size * board.size) {
-                        cout << "Koniec Gry: Wszystkie komorki chore" << endl;
-                        duration.delay(5, window);
-                        window.close();
-                        break;
-                    }
-
-                    deltaTime = clock.restart().asSeconds();
-                    allTime += deltaTime;
-
-                    duration.delay(duration.holdprocess, window);
-                    board.update(board.size, currentround, deltaTime, allTime, window, duration.infectionPercent, duration.infectedToImmune, duration.immuneCooldown);
-                    window.clear();
-                    board.drawBoard(window);
-                    window.display();
-                    currentround++;
-                }
-                
             }
+
+            if (currentround == 1 && !gameStarted && !menuOpen) {
+                window.clear();
+                board.calculateboardSize(window);
+                board.drawBoard(window);
+                window.display();
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                    board.handleClick(currentround, window);
+                    gameStarted = true;
+                    currentround = 2;
+                }     
+            }
+                
+            if (currentround != 1 && currentround <= duration.maxround && !menuOpen) {
+                if (board.countCells(Health, board.size) == board.size * board.size) {
+                    cout << "Koniec Gry: Wszystkie komorki zdrowe" << endl;
+                    duration.delay(5, window);
+                    window.close();
+                    break;
+                }
+
+                if (board.countCells(Infected, board.size) == board.size * board.size) {
+                    cout << "Koniec Gry: Wszystkie komorki chore" << endl;
+                    duration.delay(5, window);
+                    window.close();
+                    break;
+                }
+
+                deltaTime = clock.restart().asSeconds();
+                allTime += deltaTime;
+
+                duration.delay(duration.holdprocess, window);
+                board.update(board.size, currentround, deltaTime, allTime, window, duration.infectionPercent, duration.infectedToImmune, duration.immuneCooldown);
+                window.clear();
+                board.drawBoard(window);
+                window.display();
+                currentround++;
+            }    
         }
 
         window.clear();
